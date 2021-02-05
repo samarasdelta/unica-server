011019b7d4c8592487cde536181e07dbf1efb303
DimitriosSamaras <samarasdelta@gmail.com>
2021-02-05 22:43:58 +0200

    establisg conn w/ server & database 'unicadb'

--- api/index.js
+++ api/index.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-const projects = require("./data.json");
+//Status check
 const status = require("./status");
-
 router.use("/status", status);
 
+//const pool = require("../app");
+
 router.get("/projects", (req, res) => {
-    res.send(projects.papers);
+    console.log(pool,"pool");
+    pool.query('SELECT * FROM unica_projects ', function (error, results) {
+        if (error) throw error;
+        console.log('The solution is: ', results);
+        res.send(results);
+    }); 
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const SERVER_PORT = process.env.PORT || 4000;
 const express = require("express");
-const path = require("path");
 const cors = require("cors");
+const mysql = require("mysql"); 
 
 const app = express();
 const api = require("./api");
@@ -9,10 +9,27 @@ const api = require("./api");
 app.use(express.json());
 app.use(cors());
 
-/**
- * register routes
- */
+//Mysql
+const pool = mysql.createPool({
+    host     : "localhost",
+    user     : "root",
+    password : "",
+    database : "unicadb",
+    connectionLimit : 10
+});
+
+//Register routes
+app.get("/projects", (req, res) => {
+    pool.query('SELECT * FROM projects ', function (error, results) {
+        if (error) throw error;
+        console.log('The solution is: ', results);
+        res.send(results);
+    }); 
+});
+
+module.exports = pool;
 
 app.use("/api", api);
 
+//Listen on port
 app.listen(SERVER_PORT, () => console.log(`Server running on ${SERVER_PORT}`));
