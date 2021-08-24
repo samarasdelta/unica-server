916b017a696f9c4396581fe72fcb0ec2d54b98f0
zummashu <samarasdelta@gmail.com>
2021-08-24 15:37:28 +0300

    deleted endpoint migration

--- api/projects.js
+++ api/projects.js
@@ -93,7 +93,7 @@ router.delete("/:projectId", (req, res) => {
 // get project to be deleted
 router.get("/deleted", (req, res) => {
   pool.query(
-    `SELECT * FROM projects WHERE projectDeleted="1" ORDER BY projectId DESC`,
+    `SELECT * FROM projects WHERE projectDeleted="1" ORDER BY projectDateCreated DESC`,
     (err, results) => {
       if (err) res.status(500).send({ error: err.message });
 
