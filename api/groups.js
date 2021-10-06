ea35d9b5815edaa285a382b18f677498dc9e026e
zummashu <samarasdelta@gmail.com>
2021-10-06 20:59:57 +0300

    groups endpoint created v2

--- api/groups.js
+++ api/groups.js
@@ -14,5 +14,29 @@ router.get("/", (req, res) => {
   });
 });
 
+// create group
+router.post("/", (req, res) => {
+  pool.query(
+    `INSERT INTO groups(groupTitle) VALUES ('${req.body.title}') `,
+    (error, results) => {
+      // console.log(results.insertId);
+      if (error) {
+        // console.log('Error message: ', error.message);
+        res.status(500).json({
+          error: error.message,
+        });
+        throw error;
+      }
+      pool.query(
+        `SELECT * FROM groups WHERE groupId=${results.insertId}`,
+        (error1, results1) => {
+          if (error1) throw error;
+          res.send(results1);
+        }
+      );
+    }
+  );
+});
+
 // export
 module.exports = router;
