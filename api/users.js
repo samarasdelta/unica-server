9e824add8bc89dd9a4033c1f9c8838bda4969c3c
zummashu <samarasdelta@gmail.com>
2021-11-24 19:46:36 +0200

    user endpoint works and posting/creating user

--- api/users.js
+++ api/users.js
@@ -14,29 +14,29 @@ router.get("/", (req, res) => {
   });
 });
 
-// // create group
-// router.post("/", (req, res) => {
-//   pool.query(
-//     `INSERT INTO users(userFirstName, userSurName) VALUES ('${req.body.title}') `,
-//     (error, results) => {
-//       // console.log(results.insertId);
-//       if (error) {
-//         // console.log('Error message: ', error.message);
-//         res.status(500).json({
-//           error: error.message,
-//         });
-//         throw error;
-//       }
-//       pool.query(
-//         `SELECT * FROM groups WHERE groupId=${results.insertId}`,
-//         (error1, results1) => {
-//           if (error1) throw error;
-//           res.send(results1);
-//         }
-//       );
-//     }
-//   );
-// });
+// create group
+router.post("/", (req, res) => {
+  pool.query(
+    `INSERT INTO users(userFirstName, userSurName, userEmail) VALUES ('${req.body.fname}', '${req.body.sname}' ,'${req.body.email}') `,
+    (error, results) => {
+      // console.log(results.insertId);
+      if (error) {
+        // console.log('Error message: ', error.message);
+        res.status(500).json({
+          error: error.message,
+        });
+        throw error;
+      }
+      pool.query(
+        `SELECT * FROM users WHERE userId=${results.insertId}`,
+        (error1, results1) => {
+          if (error1) throw error;
+          res.send(results1);
+        }
+      );
+    }
+  );
+});
 
 // export
 module.exports = router;
