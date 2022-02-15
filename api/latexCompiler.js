6b49996497575f85d56fed5a6676837eec90abbf
zummashu <samarasdelta@gmail.com>
2022-02-15 14:30:28 +0200

    post latex returns a pdf link

--- api/latexCompiler.js
+++ api/latexCompiler.js
@@ -1,3 +1,4 @@
+const { v4: uuidv4 } = require("uuid");
 const express = require("express");
 const { writeFile, readFile } = require("fs");
 const { exec } = require("child_process");
@@ -25,7 +26,7 @@ const writeFileSync = async (where, what) => {
 
 const compileLatex = async (texFile) => {
   return new Promise((resolve, reject) => {
-    exec(`./laton ${texFile}`, (error) => {
+    exec(`./laton -o public ${texFile}`, (error, stdout, stderr) => {
       if (error) reject(error);
 
       resolve("Success!");
@@ -45,6 +46,19 @@ const compileTemplate = async (pathToTemplate) => {
     );
   });
 };
+
+router.get("/public/:file", async (req, res) => {
+  const { file } = req.params;
+  try {
+    const pdfData = await readFileSync(`./public/${file}`);
+    res.contentType("application/pdf");
+    res.send(pdfData);
+  } catch (e) {
+    res.status(500).json({
+      error: e.message,
+    });
+  }
+});
 
 router.get("/:template", async (req, res) => {
   const { template } = req.params;
@@ -73,16 +87,19 @@ router.post("/", (req, res) => {
     req.on("end", async () => {
       try {
         const latexCode = req.text;
+        const name = uuidv4();
+        await writeFileSync(`./tex/${name}.tex`, latexCode);
 
-        await writeFileSync("./tex/main.tex", latexCode);
-
-        // spawn process to compile it
-        await compileLatex("./tex/main.tex");
+        // spawn process to to compile in latex
+        await compileLatex(`./tex/${name}.tex`);
         // return pdf
-        const filePath = "./main.pdf";
-        const pdfData = await readFileSync(filePath);
-        res.contentType("application/pdf");
-        res.send(pdfData);
+        const filePath = `./${name}.pdf`;
+        res.json({
+          pdf: `http://localhost:4000/api/latex/public/${name}.pdf`,
+        });
+        // const pdfData = await readFileSync(filePath);
+        // res.contentType("application/pdf");
+        // res.send(pdfData);
       } catch (e) {
         res.status(500).json({
           error: e.message,
