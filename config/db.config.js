511a2b6e6cc52601fd4507f3c51ab62453ef9b99
zummashu <samarasdelta@gmail.com>
2022-06-25 18:06:05 +0300

    db config changes for zafora

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,10 @@
 module.exports = {
   HOST: "localhost",
+  // HOST: "83.212.19.218",
   // SOCKET: "/zstorage/home/ece00792/mysql/run/mysql.sock",
   USER: "root",
   PASS: "",
+  // PASS: "root",
   DB: "unicadb",
   CONLIMIT: "10",
 };
