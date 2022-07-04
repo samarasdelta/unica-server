c37bee7ed7961d456aad7d695a3e08527cec4545
zummashu <samarasdelta@gmail.com>
2022-07-04 15:34:19 +0300

    db.config for heroku

--- config/db.config.js
+++ config/db.config.js
@@ -1,9 +1,14 @@
 module.exports = {
-  HOST: "localhost",
+  HOST: "b797c3a2cc660f",
+  // HOST: "localhost",
   // HOST: "83.212.19.218",
   // SOCKET: "/zstorage/home/ece00792/mysql/run/mysql.sock",
-  USER: "root",
-  PASS: "",
+  USER: "8e5e4d90",
+  // USER: "root",
+  PASS: "eu-cdbr-west-03.cleardb.net",
+  // PASS: "",
   // PASS: "root",
   DB: "unicadb",
 };
+
+// mysql://b797c3a2cc660f:8e5e4d90@eu-cdbr-west-03.cleardb.net/heroku_42b9e19a38ed999?reconnect=true
