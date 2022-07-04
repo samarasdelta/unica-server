33a410838b106eb516a2d73643a16c59d78f4264
zummashu <samarasdelta@gmail.com>
2022-07-04 16:19:40 +0300

    commenting out file

--- config/db.config.js
+++ config/db.config.js
@@ -1,14 +1,17 @@
 module.exports = {
+  // heroku config
+
   HOST: "eu-cdbr-west-03.cleardb.net",
-  // HOST: "localhost",
-  // HOST: "83.212.19.218",
-  // SOCKET: "/zstorage/home/ece00792/mysql/run/mysql.sock",
   USER: "b797c3a2cc660f",
-  // USER: "root",
   PASS: "8e5e4d90",
-  // PASS: "",
-  // PASS: "root",
   DB: "heroku_42b9e19a38ed999",
+
+  // localhost config
+
+  // HOST: "localhost",
+  // USER: "root",
+  // PASS: "",
+  // DB: "unicadb",
 };
 
 // mysql://b797c3a2cc660f:8e5e4d90@eu-cdbr-west-03.cleardb.net/heroku_42b9e19a38ed999?reconnect=true
