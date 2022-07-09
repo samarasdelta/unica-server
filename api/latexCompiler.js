05996ba8ed1aea0c6a0787fc4be9a319b334f9d1
zummashu <samarasdelta@gmail.com>
2022-07-09 15:20:52 +0300

    compile

--- api/latexCompiler.js
+++ api/latexCompiler.js
@@ -1,6 +1,7 @@
-const { v4: uuidv4 } = require("uuid");
+// const { v4: uuidv4 } = require("uuid");
 const express = require("express");
-const { writeFile, readFile } = require("fs");
+// const { writeFile, readFile } = require("fs");
+const { readFile } = require("fs");
 const { exec } = require("child_process");
 
 const router = express.Router();
@@ -14,30 +15,30 @@ const readFileSync = async (filePath) => {
   });
 };
 
-const writeFileSync = async (where, what) => {
-  return new Promise((resolve, reject) => {
-    const data = new Uint8Array(Buffer.from(what));
-    writeFile(where, data, (err) => {
-      if (err) reject(err);
-      resolve("File written!");
-    });
-  });
-};
+// const writeFileSync = async (where, what) => {
+//   return new Promise((resolve, reject) => {
+//     const data = new Uint8Array(Buffer.from(what));
+//     writeFile(where, data, (err) => {
+//       if (err) reject(err);
+//       resolve("File written!");
+//     });
+//   });
+// };
 
-const compileLatex = async (texFile) => {
-  return new Promise((resolve, reject) => {
-    exec(`laton -o public ${texFile}`, (error) => {
-      if (error) reject(error);
+// const compileLatex = async (texFile) => {
+//   return new Promise((resolve, reject) => {
+//     exec(`./laton -o public ${texFile}`, (error) => {
+//       if (error) reject(error);
 
-      resolve("Success!");
-    });
-  });
-};
+//       resolve("Success!");
+//     });
+//   });
+// };
 
 const compileTemplate = async (pathToTemplate) => {
   return new Promise((resolve, reject) => {
     exec(
-      `./laton -o ./templates/${pathToTemplate}/main.pdf ./templates/${pathToTemplate}/main.tex ./templates/${pathToTemplate}/*`,
+      `./laton ./templates/${pathToTemplate}/main.tex ./templates/${pathToTemplate}/*`,
       (error) => {
         if (error) reject(error);
 
@@ -65,7 +66,7 @@ router.get("/:template", async (req, res) => {
   try {
     await compileTemplate(template);
     // return pdf
-    const filePath = `./templates/${template}/main.pdf`;
+    const filePath = `./templates/${template}/main.tex`;
     const pdfData = await readFileSync(filePath);
     res.contentType("application/pdf");
     res.send(pdfData);
@@ -87,43 +88,17 @@ router.post("/", (req, res) => {
     req.on("end", async () => {
       try {
         const latexCode = req.text;
-        const name = uuidv4();
-        await writeFileSync(`./tex/${name}.tex`, latexCode);
+        // const name = uuidv4();
+        // await writeFileSync(`./tex/${name}.tex`, latexCode);
 
         // spawn process to to compile in latex
-        await compileLatex(`./tex/${name}.tex`);
+        // await compileLatex(`./tex/${name}.tex`);
         // return pdf
-        const filePath = `./${name}.pdf`;
-        res.json({
-          pdf: `https://unica-server.vercel.app/api/latex/public/${filePath}`,
-        });
-      } catch (e) {
-        res.status(500).json({
-          error: e.message,
-        });
-      }
-    });
-  }
-});
-
-router.post("/tex", (req, res) => {
-  if (req.is("text/*")) {
-    req.text = "";
-    req.setEncoding("utf8");
-    req.on("data", (chunk) => {
-      req.text += chunk;
-    });
-
-    req.on("end", async () => {
-      try {
-        const latexCode = req.text;
-        const name = uuidv4();
-        await writeFileSync(`./templates/${name}.tex`, latexCode);
-
-        const texLink = `https://unica-server.vercel.app/api/templates/${name}.tex`;
+        // const filePath = `./${name}.pdf`;
+        const filePath = `${latexCode}`;
 
         res.json({
-          hallo: texLink,
+          pdf: `https://latexonline.cc/compile?text=${filePath}`,
         });
       } catch (e) {
         res.status(500).json({
